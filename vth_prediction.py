3627b028607ff8e6de176e130c9db0eb11e4147a
Shatrunjay Prabhat <55319863+Prabhat1004@users.noreply.github.com>
2021-08-17 22:00:26 +0530

    Update vth_prediction.py

--- vth_prediction.py
+++ vth_prediction.py
@@ -1,5 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fhand = open(r"C:\Users\shatrunjay\Desktop\Thesis\ML Model\dataset1.csv", 'r')
+data  = list()
+count = 0
+for line in fhand :
+    if (count > 0) :
+        temp = [float(item) for item in line.split(',')]
+        data.append(temp)   
+    count = count + 1
 
+dataset_unscaled = np.array(data)
 
